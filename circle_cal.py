8e2780acb93c5b3f52fe2d3ddc6fca29c63d8b39
Andy <ayubxonayub54@gmail.com>
2025-10-07 11:04:47 -0700

    Week2 practice(Circle calculator)

---
+++ circle_cal.py
@@ -0,0 +1,7 @@
+r = float(input('Enter the radius: '))
+pi = 3.14159
+area = pi * r**2
+circumference = 2*pi*r 
+
+print(f'Area equals to {area:.2f}')
+print(f'Circumference equals to {circumference:.2f}')
